e4b17b93bdf790c872bd9618b79b5546df528eb6
unknown <klam9986@bths.edu>
2020-06-03 15:47:41 -0400

    Updated buttons edit and save cancel

--- src/FormEdit.js
+++ src/FormEdit.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class FormEdit extends Component {
   constructor(props) {
     super(props);
-    this.state = {firstName: props.first, lastName: props.last, editName: props.edit}
+    this.state = {
+        firstName: props.first, 
+        lastName: props.last, 
+        edit: false,
+    }
     this.handleNameChange = this.handleNameChange.bind(this);
   }
 
@@ -13,16 +17,35 @@ class FormEdit extends Component {
       });
   }
 
-  editButton(events) {
+  edit(events) {
+      this.setState({
+          edit: !this.state.edit
+      });
+  }
+
+  saveButton(events) {
       
   }
 
-
   render() {
       return <>
-        <input type= 'text' name= "firstName" onChange= {this.handleNameChange} />
-        <input type= 'text' name= "lastName" onChange= {this.handleNameChange} />
-        <button onClick={() => this.editButton(this.state)}>edit</button>
+        <div>
+        {this.state.edit ? 
+        <div>
+          <input type= 'text' name= "firstName" value= {this.state.firstName} onChange= {this.handleNameChange} />
+          <input type= 'text' name= "lastName" value= {this.state.lastName} onChange= {this.handleNameChange} />
+          <button onClick={() => this.edit(this.state)}>Save</button>
+          <button onClick={() => this.edit(this.state)}>Cancel</button>
+        </div>
+        : <div>
+            <p>First Name: {this.state.firstName}</p> 
+            <p>Last Name: {this.state.lastName}</p>
+            <button onClick={() => this.edit(this.state)}>edit</button>
+        </div>
+        }
+
+        </div>
+
       </>
   }
 }
